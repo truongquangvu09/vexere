a2c0edae2234720e5890da7c12f2f5dca435cc9d
quangzu <82556917+truongquangvu09@users.noreply.github.com>
2023-04-14 01:21:35 +0700

    upload files

--- router/user.router.js
+++ router/user.router.js
@@ -13,11 +13,25 @@ const userRouter = express.Router();
 
 userRouter.post("/register", createUSer);
 userRouter.post("/login", login);
+
+//upload files
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/avatars"); // set up cho can luu file
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() +"_"+ file.originalname); //dat lai ten cho file
+  },
+});
+const upload = multer({ storage: storage });
+userRouter.post("/upload-avatar", upload.single("avatar"), (req, res) => {
+  res.status(200).send("tinh nang run ok");
+});
 userRouter.get("/", getAllUsers);
 userRouter.get("/:id", getDetailUsers);
 userRouter.post("/:id", updateUsers);
 userRouter.delete("/:id", deleteUser);
-
 
 module.exports = {
   userRouter,
